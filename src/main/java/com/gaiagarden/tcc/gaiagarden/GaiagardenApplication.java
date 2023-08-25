5ba47ad219c9c6534c5e96cf939470e8a4168697
Eric D Santos <ericbucky47@gmail.com>
2023-08-25 11:58:19 -0300

    iniciação do projeto

---
+++ src/main/java/com/gaiagarden/tcc/gaiagarden/GaiagardenApplication.java
@@ -0,0 +1,13 @@
+package com.gaiagarden.tcc.gaiagarden;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GaiagardenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GaiagardenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gaiagarden/tcc/gaiagarden/GaiagardenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gaiagarden.tcc.gaiagarden;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GaiagardenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
